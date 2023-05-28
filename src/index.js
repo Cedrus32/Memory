540641157611bc7a09771114678d9ef400ed76e5
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-05-27 19:49:23 -0500

    Remove scripts/icons.js, move images into icons directory

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Main from './components/Main';
 
 import './styles/style.css';
 import './styles/reset.css';
-import iconsArray from './scripts/icons.js';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Main/>);
