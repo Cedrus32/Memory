e2f69f49b8d20fe6290196a97fb0be20f90d55fd
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-05-29 06:16:14 -0500

    Remove dist folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import icons from './scripts/icons';
 import fonts from './scripts/fonts';
-
 import Main from './components/Main';
-
 import './styles/reset.css';
 import './styles/style.css';
 
